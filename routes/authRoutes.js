e16f0bd08547b63c43a317ee58fcf0550ae5aa12
rickychang0989 <ricky.chang@newtype.games>
2022-09-27 21:20:26 +0800

    integrate the h365 third party login mechanism

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,34 +1,73 @@
 const mongoose = require("mongoose");
 const User = mongoose.model('user');
 const passport = require('passport');
+const axios = require('axios').default;
 
 module.exports = app => {
-    app.post('/register/password', async (req, res) => {
-        console.log(req.body.username);
-        console.log(req.body.password);
-        const { username, password } = req.body;
-        const existingUser = await User.findOne({
-            name: username
-        });
-        if(existingUser){
-            return res.json({ message: 'Username has been occupied.'});
+    app.get('/auth/h365', function(req, res){
+        // const queryString = encodeURIComponent(`?redirect_path=/&merchantId=${process.env.MERCHANT_ID}&serviceId=${process.env.SERVICEID}`);
+        res.redirect((`${process.env.THIRD_PARTY_LOGIN_URL}?callback=${process.env.THIRD_PARTY_LOGIN_CALLBACK_URL}&merchantId=${process.env.MERCHANT_ID}&serviceId=${process.env.SERVICE_ID}`));
+    });
+
+    app.get('/auth/h365/callback', async function(req, res){
+        const { token } = req.query;
+        try{
+            const result = await axios.get(`${process.env.H365_API_URL}/user/get-user-info?merchantId=${process.env.MERCHANT_ID}&serviceId=${process.env.SERVICE_ID}`,{
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            }); 
+            console.log(result.data);
+            if(result.status != 200 ){
+                throw result.status + "";
+            }
+
+            if(result.data.code < 0){
+                throw result.data.message;
+            }
+
+            // TODO: using following information to register a new user if not found;
+            // TODO: setting req.user
+            const { userSn, uuid, userEmail } = result;
+
+            res.json({ message: "ok"});
+        }catch(e){
+            if(typeof(e) == 'string'){
+                res.json({message: e});
+                return; 
+            }
+            console.error(e);
+            res.json({message: e.message});
         }
+       
+    });
 
-        const user = await new User({
-            googleID:"12345",
-            name: username,
-            email: "",
-            urlImg: '',
-            password: password ,
-        }).save();
+    // app.post('/register/password', async (req, res) => {
+    //     console.log(req.body.username);
+    //     console.log(req.body.password);
+    //     const { username, password } = req.body;
+    //     const existingUser = await User.findOne({
+    //         name: username
+    //     });
+    //     if(existingUser){
+    //         return res.json({ message: 'Username has been occupied.'});
+    //     }
 
-        return res.json(user);
-    })
+    //     const user = await new User({
+    //         googleID:"12345",
+    //         name: username,
+    //         email: "",
+    //         urlImg: '',
+    //         password: password ,
+    //     }).save();
 
-    app.post('/login/password', passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    }));
+    //     return res.json(user);
+    // })
+
+    // app.post('/login/password', passport.authenticate('local', {
+    //     successRedirect: '/',
+    //     failureRedirect: '/login'
+    // }));
 
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile','email'],

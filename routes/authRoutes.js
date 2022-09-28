b93c6cc6da48af855721784b53abf72ef58f5a33
rickychang0989 <ricky.chang@newtype.games>
2022-09-28 10:39:18 +0800

    integrate create account and express session

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,31 +5,59 @@ const axios = require('axios').default;
 
 module.exports = app => {
     app.get('/auth/h365', function(req, res){
-        // const queryString = encodeURIComponent(`?redirect_path=/&merchantId=${process.env.MERCHANT_ID}&serviceId=${process.env.SERVICEID}`);
         res.redirect((`${process.env.THIRD_PARTY_LOGIN_URL}?callback=${process.env.THIRD_PARTY_LOGIN_CALLBACK_URL}&merchantId=${process.env.MERCHANT_ID}&serviceId=${process.env.SERVICE_ID}`));
     });
 
     app.get('/auth/h365/callback', async function(req, res){
         const { token } = req.query;
         try{
-            const result = await axios.get(`${process.env.H365_API_URL}/user/get-user-info?merchantId=${process.env.MERCHANT_ID}&serviceId=${process.env.SERVICE_ID}`,{
+            const response = await axios.get(`${process.env.H365_API_URL}/user/get-user-info?merchantId=${process.env.MERCHANT_ID}&serviceId=${process.env.SERVICE_ID}`,{
                 headers: {
                     'Authorization': `Bearer ${token}`,
                 }
             }); 
-            console.log(result.data);
-            if(result.status != 200 ){
+
+            if(response.status != 200 ){
                 throw result.status + "";
             }
 
-            if(result.data.code < 0){
+            if(response.data.code < 0){
                 throw result.data.message;
             }
 
-            // TODO: using following information to register a new user if not found;
-            // TODO: setting req.user
-            const { userSn, uuid, userEmail } = result;
+            const { userSn, uuid } = response.data.data;
 
+            let user = await User.findOne({
+                h365ID: uuid,
+            });
+    
+            if(!user){
+                console.log(`user[${uuid}] not found, create new one`);
+                user = await new User({
+                    googleID:"",
+                    h365ID: uuid,
+                    name: uuid,
+                    email: "",
+                    urlImg: '',
+                    password: '',
+                }).save();
+            }
+            const p = new Promise((resolve, reject)=> {
+                req.logIn(user, (err)=> {
+                    if (err) { 
+                        console.log(err) 
+                        reject(err);
+                    }
+                    resolve();
+                });
+            });
+
+            await p;
+
+            if(process.env.THIRD_PARTY_LOGIN_REDIRECT_URL){
+                res.redirect(process.env.THIRD_PARTY_LOGIN_REDIRECT_URL);
+                return;
+            }
             res.json({ message: "ok"});
         }catch(e){
             if(typeof(e) == 'string'){
@@ -39,35 +67,7 @@ module.exports = app => {
             console.error(e);
             res.json({message: e.message});
         }
-       
     });
-
-    // app.post('/register/password', async (req, res) => {
-    //     console.log(req.body.username);
-    //     console.log(req.body.password);
-    //     const { username, password } = req.body;
-    //     const existingUser = await User.findOne({
-    //         name: username
-    //     });
-    //     if(existingUser){
-    //         return res.json({ message: 'Username has been occupied.'});
-    //     }
-
-    //     const user = await new User({
-    //         googleID:"12345",
-    //         name: username,
-    //         email: "",
-    //         urlImg: '',
-    //         password: password ,
-    //     }).save();
-
-    //     return res.json(user);
-    // })
-
-    // app.post('/login/password', passport.authenticate('local', {
-    //     successRedirect: '/',
-    //     failureRedirect: '/login'
-    // }));
 
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile','email'],
